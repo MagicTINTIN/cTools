1b7096b26b0ef29a94620b07bb59dd916799790b
MagicTINTIN <magictintin@proton.me>
2024-09-14 19:43:05 +0200

    fix < -> <= + '\n' -> ''

--- copyUnderCondition.cc
+++ copyUnderCondition.cc
@@ -2,6 +2,43 @@
 #include <iostream>
 #include <fstream>
 
+std::string replaceChar(std::string s, char c1, char c2)
+{
+    for (long unsigned int i = 0; i < s.length(); i++) 
+    {
+        if (s[i] == c1)
+            s[i] = c2;
+        // else if (s[i] == c2)
+        //     s[i] = c1;
+    }
+    return s;
+}
+
+std::string deleteChar(std::string s, char c1)
+{
+    std::string ret("");
+    for (long unsigned int i = 0; i < s.length(); i++) 
+    {
+        if (s[i] != c1)
+            ret+=s[i];
+    }
+    return ret;
+}
+
+std::string string_to_hex(const std::string& input)
+{
+    static const char hex_digits[] = "0123456789ABCDEF";
+
+    std::string output;
+    output.reserve(input.length() * 2);
+    for (unsigned char c : input)
+    {
+        output.push_back(hex_digits[c >> 4]);
+        output.push_back(hex_digits[c & 15]);
+    }
+    return output;
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc != 4) {
@@ -17,10 +54,12 @@ int main(int argc, char const *argv[])
     while (std::getline(infile, line))
     {
         lines++;
+        std::string cleaned = deleteChar(line, 13);// deleteChar(, ' ');
         // std::cout << line << "\n";
-        if (line.size() < maxAllowed) {
-            outfile << line << "\n";
+        if (cleaned.size() <= maxAllowed) {
+            outfile << cleaned << "\n";
             copiedLines++;
+            // std::cout << cleaned << ": 0x" << string_to_hex(cleaned) << "\n";
         }
     }
     printf("Finished. %d/%d lines copied.\n", copiedLines, lines);
