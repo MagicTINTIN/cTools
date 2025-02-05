8797e6245f66bfa101615d1a9b42e95f38595bbc
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-02-05 19:34:52 +0100

    jpo: home-made chaser

--- arduino/jpo.ino
+++ arduino/jpo.ino
@@ -1,24 +1,53 @@
 #include <LiquidCrystal.h>
 
-// Initialize the LCD with the pins connected to RS, E, D4, D5, D6, D7
 LiquidCrystal lcd(10, 11, 2, 3, 4, 5);
 const int LEFT_LED = 6;
 const int RIGHT_LED = 9;
 const int BUTTON = 12;
+const char* topString = "Bienvenue aux JPO de l'INSA Toulouse !           ";
+const int topStringLength = 42;
+
+int chaserOffset = 0;
+
+const int MAX_TIME = 1*60;
+int remainingSeconds = MAX_TIME;
+const int threshold = 50;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(BUTTON, INPUT);
     pinMode(LEFT_LED, OUTPUT);
     pinMode(RIGHT_LED, OUTPUT);
-    lcd.begin(16, 1); // 16x2 LCD
-    lcd.print("Bienvenue aux JPO de l'INSA Toulouse ! Bienvenue aux JPO de l'INSA Toulouse !");
+    lcd.begin(16, 2);
+    // lcd.print("1234567890123456abcdefghijklmnopz");
+    // lcd.print("1234567890123456abcdefghijklmnopz");
+    lcd.setCursor(0,1);
+    lcd.print("RESTANT:");
     delay(1000);
+}
+
+void writeCharAt(char c, int x, int y) {
+  lcd.setCursor(x,y);
+  lcd.write(c);
+}
+
+void chaserUpdate() {
+  chaserOffset = (chaserOffset+1) % topStringLength;
+  for (int x = 0; x < 16; x++) {
+    writeCharAt(topString[(chaserOffset+x)%topStringLength],x,0);
+  }
+}
+
+void timeUpdate(int seconds) {
 }
 
 void refresh() {
   int buttonState = digitalRead(BUTTON);
   if (buttonState == HIGH) {
+    remainingSeconds = MAX_TIME;
+  }
+  
+  if (remainingSeconds < threshold) {
     digitalWrite(LEFT_LED, HIGH);
     digitalWrite(RIGHT_LED, HIGH);
   } else {
@@ -28,7 +57,9 @@ void refresh() {
 }
 
 void waitWhileRefresh(int time, int precision) {
-  lcd.scrollDisplayLeft();
+  //lcd.scrollDisplayLeft();
+
+  chaserUpdate();
   for (int i=0; i<precision;i++) {
     delay(time/precision);
     refresh();
@@ -36,9 +67,13 @@ void waitWhileRefresh(int time, int precision) {
 }
 
 void loop() {
-  
+  for (int i=0;i<5;i++) {
   digitalWrite(LED_BUILTIN, HIGH);
   waitWhileRefresh(200,10);
   digitalWrite(LED_BUILTIN, LOW);
   waitWhileRefresh(200,10);
+  }
+  if (remainingSeconds > 0)
+    remainingSeconds--;
+  timeUpdate(remainingSeconds);
 }
